09160aaa749f21a50940bcc902b6d1a204a934b2
Kamil-IKL <161152970+Kamil-IKL@users.noreply.github.com>
2024-12-10 19:21:57 +0500

    Add files via upload

---
+++ M01_DZ-03.py
@@ -0,0 +1,8 @@
+name = 'Камиль'
+print('Имя:', name, type(name))
+age = int(50)
+print('Возраст:', age, type(age))
+new_age = age + 2
+print('Новый возраст:', new_age, type(new_age))
+is_student = True
+print('Я студент?:', is_student, type(is_student))
