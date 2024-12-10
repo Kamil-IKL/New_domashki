fdee2b4fc857f5d6c4964cd1311c716b84fba1eb
Kamil-IKL <161152970+Kamil-IKL@users.noreply.github.com>
2024-12-10 19:49:32 +0500

    Add files via upload

---
+++ M01_DZ-04.py
@@ -0,0 +1,8 @@
+NumberOfCompletedDz = 12
+print('Количество выполненных ДЗ: ', NumberOfCompletedDz)
+NumberOfHoursSpent = 1.5
+print('Количество затраченных часов: ', NumberOfHoursSpent)
+NameCourse = 'Python'
+print('Наименование курса: ', NameCourse)
+TimePerTask = NumberOfHoursSpent / NumberOfCompletedDz # время на одно задание
+print(f'Время потраченное на одно задание = {TimePerTask * 60} минут')
