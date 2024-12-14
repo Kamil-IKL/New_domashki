086698c7af40b272ce013c4e46f2acd192ee9d23
Kamil-IKL <161152970+Kamil-IKL@users.noreply.github.com>
2024-12-14 15:42:38 +0500

    Update module_1_3.py

--- module_1_3.py
+++ module_1_3.py
@@ -1,6 +1,8 @@
 name = 'Камиль'
-print(name)
-name = 'Urban'
-print(name)
-date_of_birth = 'April 2022' # стиль Snake case
-dateOfBirth = 'April 2022' # стиль Camel case
+print('Имя:', name, type(name))
+age = int(50)
+print('Возраст:', age, type(age))
+new_age = age + 2
+print('Новый возраст:', new_age, type(new_age))
+is_student = True
+print('Я студент?:', is_student, type(is_student))
