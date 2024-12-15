2b14a73e54f4f06bad4a56bddb394a6a2765220e
Kamil-IKL <161152970+Kamil-IKL@users.noreply.github.com>
2024-12-15 18:06:12 +0500

    Update M02_DZ-03.py

--- M02_DZ-03.py
+++ M02_DZ-03.py
@@ -1,7 +1,15 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+'''Замечания;
+   По заданию при встрече с отрицательным числом следует прервать цикл'''
+# i = 0
+# while i < len(my_list):  # True
+#     if my_list[i] > 0:
+#         print('index', i, '=', my_list[i])
+#     i += 1
+# print(f'Проверены все {len(my_list)} элементов списка')
+
+'''с учетом замечания при проверке'''
 i = 0
-while i < len(my_list):  # True
-    if my_list[i] > 0:
-        print('index', i, '=', my_list[i])
+while i < len(my_list) and my_list[i] >= 0:
+    print('index', i, '=', my_list[i])
     i += 1
-print(f'Проверены все {len(my_list)} элементов списка')
