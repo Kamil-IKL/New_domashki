e10a97a59b4a720205e34cf395dc2c8bd95967ac
Kamil-IKL <161152970+Kamil-IKL@users.noreply.github.com>
2024-12-23 17:23:39 +0500

    Update M03_DZ-05.py

--- M03_DZ-05.py
+++ M03_DZ-05.py
@@ -3,11 +3,17 @@ def get_multiplied_digits(number):
     first = int(str_number[0]) # Первая цифра в числовом представлении
     # print(first, type(first))
     # print(len(str_number))
+    
+    # Проверяю на окончание параметра number, если в конце ноль, то убираю ноль
+    if int(str_number[-1]) == 0:
+        str_number = str_number.rstrip("0")
+        # print(f'проверка метода .rstrip("0") {str_number}')
+    
     if len(str_number) > 1:
         # Рекурсия этой функции для оставшихся цифр
         return first * get_multiplied_digits(int(str_number[1:]))
     else:
-        # Возвращаем, если осталась одна цифра
+        # Возвращаю, если осталась одна цифра
         return int(str_number[0])
 
 
