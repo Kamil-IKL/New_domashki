27fc460968a8b9c6f504ef52d10250a914b9cfe3
Kamil-IKL <161152970+Kamil-IKL@users.noreply.github.com>
2024-12-28 19:52:18 +0500

    Add files via upload

---
+++ module_4_2.py
@@ -0,0 +1,11 @@
+def test_function():
+    def inner_function():
+        print('Я в области видимости функции test_function')
+
+    inner_function()
+
+
+# inner_function()    # в данном случае выдает ошибку:
+#                     # name 'inner_function' is not defined. Did you mean: 'test_function'?
+
+test_function()
